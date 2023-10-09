a5ed7e7ef30abfbf69e23dcd7d9f678b13cbb1e2
isobelbutler <isobutler@icloud.com>
2023-10-09 16:43:53 +0100

    Schema and seed created

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -2,34 +2,18 @@ PRAGMA foreign_keys = ON;
 
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS categories (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  description TEXT
+CREATE TABLE IF NOT EXISTS images (
+  id INTEGER PRIMARY KEY,
+  image_file BLOB NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS products (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  category_id INTEGER REFERENCES categories(id),
-  quantity_per_unit TEXT,
-  unit_price NUMERIC DEFAULT 0,
-  units_in_stock INTEGER DEFAULT 0,
-  units_on_order INTEGER DEFAULT 0,
-  CHECK (unit_price>=(0)),
-  CHECK (units_in_stock>=(0)),
-  CHECK (units_on_order>=(0))
+CREATE TABLE IF NOT EXISTS image_details (
+  id INTEGER PRIMARY KEY,
+  description VARCHAR(255),
+  created_at TIME,
+  image_id INTEGER,
+  uploaded_by VARCHAR(255),
+  FOREIGN KEY (image_id) REFERENCES images (id)
 );
-
--- Help the DB quickly find products by their name later.
--- This will make searching by name faster.
--- E.g. the index will look like:
--- name  | row_id
--- Chai  | 1
--- Chang | 2
--- etc...
--- So when we say "find product with name Chai" it can jump straight to the row
--- If we include all columns we need here then SQLite will not even need to access the original table
-CREATE INDEX IF NOT EXISTS product_names ON products(name);
 
 COMMIT;
