4198ae519bda7bb15b24f7a1ecc9d5d46f8688ea
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-10-09 16:38:00 +0100

    eslint added
    Relates #2

--- test/five.test.js
+++
@@ -1,9 +0,0 @@
-const test = require("node:test");
-const assert = require("node:assert");
-const { listProducts } = require("../src/model/products.js");
-
-test("listProducts() calculates the stock value", () => {
-  const products = listProducts();
-  const product = products[0];
-  assert.equal(product.stock_value, 702);
-});
