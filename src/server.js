9f9a544e03ec2369ecfb84c0bd74a546d98b8abf
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-09 18:01:15 +0100

    basic html template added

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const server = express();
-
-const homePage = require("./routes/homepage.js");
-
+const staticHandler = express.static('public');
 const bodyParser = require("body-parser");
-server.use(bodyParser.urlencoded({ extended: false }));
 
-server.use("/", homePage);
+const galleryRoute = require("./routes/gallery");
+
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(staticHandler);
+
 // TO DO
 // - Create routes
 // - Server.use (routes)
 
+server.use('/', galleryRoute);
 module.exports = server;
