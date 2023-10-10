158d86ff4d4ac355537053eb6a2efd9480329f77
isobelbutler <isobutler@icloud.com>
2023-10-10 10:46:25 +0100

    Created test to check user image ends with valid file extension

---
+++ test/image-extension.test.js
@@ -0,0 +1,30 @@
+const test = require('node:test');
+const assert = require('node:assert');
+const { submitWork } = require('../src/model/images.js');
+
+test('User can only submit images with filenames ending in jpg, jpeg, or png', () => {
+  // Regex for valid file extensions
+  const validFileExtension = /\.(jpg|jpeg|png)$/i;
+
+  // Test with valid file extensions
+  const validFilenames = ['flowers.jpg', 'sunset.jpeg', 'beach.png'];
+  validFilenames.forEach((filename) => {
+    const work = submitWork(filename);
+    assert.equal(
+      validFileExtension.test(filename),
+      true,
+      console.log(`Expected ${filename} to have a valid extension`)
+    );
+  });
+
+  // Test with filenames having invalid extensions
+  const invalidFilenames = ['mountain.gif', 'document.txt', 'family_photo.pdf'];
+  invalidFilenames.forEach((filename) => {
+    const work = submitWork(filename);
+    assert.equal(
+      validFileExtension.test(filename),
+      false,
+      console.log(`Expected ${filename} to have an invalid extension`)
+    );
+  });
+});
