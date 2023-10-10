9efb9f15778fd3d06b735836e816a3e057643c13
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-10-10 10:23:01 +0100

    Sentry dsn set up

--- src/server.js
+++ src/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const server = express();
+const Sentry = require('@sentry/node');
+
+const sentryDSN = process.env.SENTRY_DSN
+Sentry.init({ dsn: sentryDSN });
+
+// The request handler must be the first middleware on the app
+server.use(Sentry.Handlers.requestHandler());
 
 const homePage = require("./routes/homepage.js");
 
@@ -10,5 +17,18 @@ server.use("/", homePage);
 // TO DO
 // - Create routes
 // - Server.use (routes)
+
+
+
+// The error handler must be before any other error middleware and after all controllers
+server.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+server.use(function onError(err, req, res, next) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end(res.sentry + "");
+  });
 
 module.exports = server;
