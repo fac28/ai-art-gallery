cf4b867154df0304d73ec8fd96ae330a4bb7fb7c
isobelbutler <isobutler@icloud.com>
2023-10-10 12:59:19 +0100

    Started creating server routes

--- src/model/images.js
+++ src/model/images.js
@@ -1,21 +1,41 @@
 const db = require('../database/db.js');
 
-module.exports = { submitWork, selectWork, displayWorks };
+module.exports = {
+  // submitWork,
+  // selectWork,
+  // displayWorks,
+  insertImage,
+  insertArtworkDetails,
+};
 
-
-function submitWork() {
-  console.log('Artwork submitted');
-}
-
-const select_artwork = db.prepare(/*sql*/ `
-    SELECT id, description FROM image_details
+const insert_image = db.prepare(/*sql*/ `
+  INSERT INTO images (image_file)
+  VALUES ($image_file)
+  RETURNING id
 `);
 
-function selectWork() {
-  return select_artwork.all();
-  console.log('Artwork selected');
+function insertImage() {
+  return insert_image.get();
 }
 
-function displayWorks() {
-  console.log('Beautiful!');
+const insert_artwork_details = db.prepare(/*sql*/ `
+  INSERT INTO image_details (description, image_id, uploaded_by)
+  VALUES ($description, $image_id, $uploaded_by) WHERE image_id = ?
+`);
+
+function insertArtworkDetails(image_id) {
+  return insert_artwork_details.run(image_id);
 }
+
+// const select_artwork = db.prepare(/*sql*/ `
+//     SELECT id, description FROM image_details
+// `);
+
+// function selectWork() {
+//   return select_artwork.all();
+//   console.log('Artwork selected');
+// }
+
+// function displayWorks() {
+//   console.log('Beautiful!');
+// }
