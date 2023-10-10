8da4301175820dacc2486ed4e0f9ad9123304e9c
Elena Cazan <59057287+cazanelena@users.noreply.github.com>
2023-10-10 11:04:11 +0100

    sentry release set up

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const server = express();
 const Sentry = require('@sentry/node');
 
 const sentryDSN = process.env.SENTRY_DSN
-Sentry.init({ dsn: sentryDSN });
+Sentry.init({ 
+    dsn: sentryDSN,
+    release: "ai-art-gallery@" + process.env.npm_package_version
+});
 
 // The request handler must be the first middleware on the app
 server.use(Sentry.Handlers.requestHandler());
