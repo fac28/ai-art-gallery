3b58b5af2f1fac8bc36287a79e45a3a6cd49a2b8
Yuqing Wang <44486576+yuqingwwang@users.noreply.github.com>
2023-10-12 11:24:24 +0100

    limit size and file type

--- src/routes/form.js
+++ src/routes/form.js
@@ -5,6 +5,8 @@ const { insertImage, insertArtworkDetails } = require("../model/images");
 
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
+const MAX_SIZE = 1000 * 1000 * 5; // 5 megabytes
+const ALLOWED_TYPES = ["image/jpeg", "image/png"];
 
 router.get("/", (req, res) => {
   return res.send(form());
@@ -14,21 +16,29 @@ router.post("/", upload.single("avatar"), (req, res) => {
   // req.file is the `avatar` file
   const fileImg = req.file;
 
-  // req.body will hold the text fields, if there were any
-  const { name, description } = req.body;
-  let tags = req.body.action;
-
-  if (!tags) {
-    tags = [];
+  if (!ALLOWED_TYPES.includes(fileImg.mimetype)) {
+    res.status(400).send("<h1>File upload error</h1><p>Please upload an image file</p>");
   }
 
-  const tagsString = tags.join(", ");
+  if (fileImg.size > MAX_SIZE) {
+    res.status(400).send("<h1>File upload error</h1><p>Profile picture must be < 5MB</p>");
+  } else {
+    // req.body will hold the text fields, if there were any
+    const { name, description } = req.body;
+    let tags = req.body.action;
 
-  const imageBuffer = fileImg.buffer.toString("base64");
-  const imageId = insertImage(imageBuffer).id;
-  insertArtworkDetails(description, imageId, name, tagsString);
+    if (!tags) {
+      tags = [];
+    }
 
-  res.redirect("/");
+    const tagsString = tags.join(", ");
+
+    const imageBuffer = fileImg.buffer.toString("base64");
+    const imageId = insertImage(imageBuffer).id;
+    insertArtworkDetails(description, imageId, name, tagsString);
+
+    res.redirect("/");
+  }
 });
 
 module.exports = router;
